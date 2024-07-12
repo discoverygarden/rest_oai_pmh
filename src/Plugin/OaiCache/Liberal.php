4b18d5bbd4c4c107e7a74b550301c5b62b0ddf73
Adam Vessey <adam@discoverygarden.ca>
2024-07-12 13:15:22 -0300

    Use `UNION ALL` to avoid distinct record calculation.

--- src/Plugin/OaiCache/Liberal.php
+++ src/Plugin/OaiCache/Liberal.php
@@ -74,7 +74,7 @@ class Liberal extends OaiCacheBase {
 SELECT 1
 FROM {rest_oai_pmh_record} r
 WHERE (r.entity_type = :entity_type AND r.entity_id = :entity_id)
-UNION
+UNION ALL
 SELECT 1
 FROM {rest_oai_pmh_set} s
 WHERE (s.entity_type = :entity_type AND s.set_id = :set_id)
